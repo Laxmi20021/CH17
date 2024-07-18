d904d092b614bce9384b5d559cb2ba09c8d0fd20
Laxmi Patil <laxmivpatilxworkz@gmail.com>
2024-07-18 18:34:01 +0530

    running elephant

---
+++ Elephant.java
@@ -0,0 +1,20 @@
+class Elephant {
+    public static void main(String[] args) {
+        printElephant();
+    }
+
+    public static void printElephant() {
+        System.out.println("       _  _");
+        System.out.println("     / `   `-. ");
+        System.out.println("    (    .__.'");
+        System.out.println("     |  /  \\ ");
+        System.out.println("     |  |  | ");
+        System.out.println("     |  |  | ");
+        System.out.println("     |  |  | ");
+        System.out.println("     |  |  | ");
+        System.out.println("    _|  |  |_ ");
+        System.out.println("  /`  |  |  |`\\ ");
+        System.out.println(" /    '.__.'   \\ ");
+        System.out.println(" `---'   `---'");
+    }
+}
