7f6c53d13b1523d3e78d7ed1f008428e502bdeac
Laxmi Patil <laxmivpatilxworkz@gmail.com>
2024-09-15 19:47:43 +0530

    uploaded

--- Laptop/src/com/xworkz/Laptop/External/ProviderRunner.java
+++ Laptop/src/com/xworkz/Laptop/External/ProviderRunner.java
@@ -20,41 +20,43 @@ public class ProviderRunner {
 BookingProvider bookingProvider=new BookingProvider("jashb", "nsbdsvdv");
 		
 		
-        boolean ref=bookingProvider.equals(null);
+        boolean provider=bookingProvider.equals(null);
         
-      System.out.println(ref);
+      System.out.println(provider);
       System.out.println(bookingProvider.toString());
       bookingProvider.service();
       
       System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<");
       
       DellProvider DellProvider =new DellProvider("zmndb", "msnhdbd");
-        boolean ref1=DellProvider.equals(DellProvider);
-        System.out.println(ref1);
+        boolean provider0=DellProvider.equals(DellProvider);
+        System.out.println(provider0);
         System.out.println(DellProvider.toString());
         DellProvider.service();
         
         System.out.println("..................................");
         LenovoProvider lenovoProvider1 =new LenovoProvider("snbd ", "skjehg", 25);
         LenovoProvider lenovoProvider2 =new LenovoProvider("jawh", "jashdbv", 25);
-        boolean ref2=lenovoProvider1.equals(lenovoProvider2);
-        System.out.println(ref2);
+        LenovoProvider lenovoProvider3 =new LenovoProvider("Laxmi", "Lakshmi", 35);
+        boolean provider1=lenovoProvider1.equals(lenovoProvider2);
+        System.out.println(provider1);
         System.out.println(lenovoProvider1.toString());
         lenovoProvider1.service();
         
-        System.out.println("running in provider");
+        
+System.out.println("running in provider");
         
         InternetProvider internrtProvider=new InternetProvider("jhsbdd", "snbdd");
-        boolean ref3=internrtProvider.equals(internrtProvider);
-        System.out.println(ref3);
+        boolean provider2=internrtProvider.equals(internrtProvider);
+        System.out.println(provider2);
         System.out.println(internrtProvider.toString());
         internrtProvider.service();
         
         System.out.println("//////////////////////////////");
         
         MovieBookingProvider movieBookingProvider =new MovieBookingProvider("ajsbhd", "msnhhbd");
-        boolean ref4=movieBookingProvider.equals(movieBookingProvider);
-        System.out.println(ref4);
+        boolean provider3=movieBookingProvider.equals(movieBookingProvider);
+        System.out.println(provider3);
         System.out.println(movieBookingProvider.toString());
         movieBookingProvider.book();
         movieBookingProvider.cancel();
@@ -63,8 +65,8 @@ BookingProvider bookingProvider=new BookingProvider("jashb", "nsbdsvdv");
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         
         BookMyShowMovieBookingProvider bookMyShowMovieBookingProvider=new BookMyShowMovieBookingProvider("nsbdv", "snbd  ", "snd");
-        boolean ref5=bookMyShowMovieBookingProvider.equals(bookMyShowMovieBookingProvider);
-        System.out.println(ref5);
+        boolean provider4=bookMyShowMovieBookingProvider.equals(bookMyShowMovieBookingProvider);
+        System.out.println(provider4);
         System.out.println(bookMyShowMovieBookingProvider.toString());
         bookMyShowMovieBookingProvider.book();
         bookMyShowMovieBookingProvider.cancel();
@@ -73,31 +75,31 @@ BookingProvider bookingProvider=new BookingProvider("jashb", "nsbdsvdv");
         System.out.println("------------------------");
         
         AirtelInternetProvider airtelInternetProvider =new AirtelInternetProvider("smdn", "sknd");
-        boolean ref6=airtelInternetProvider.equals(airtelInternetProvider);
-        System.out.println(ref6);
+        boolean provider5=airtelInternetProvider.equals(airtelInternetProvider);
+        System.out.println(provider5);
         System.out.println(airtelInternetProvider.toString());
         airtelInternetProvider.service();
         
         System.out.println("+++++++++++++++++++++");
         
         ExtrmeAirtelInternetProvider extremeAirtelInternetProvider=new ExtrmeAirtelInternetProvider("sdn", "skhdb");
-        boolean ref7=extremeAirtelInternetProvider.equals(extremeAirtelInternetProvider);
-        System.out.println(ref7);
+        boolean provider6=extremeAirtelInternetProvider.equals(extremeAirtelInternetProvider);
+        System.out.println(provider6);
         System.out.println(extremeAirtelInternetProvider.toString());
         extremeAirtelInternetProvider.service();
         
         System.out.println("=============");
         
         Fast5gExtreme fast5gExtreme=new Fast5gExtreme("smnd", "smndb");
-        boolean ref8=fast5gExtreme.equals(fast5gExtreme);
-        System.out.println(ref8);
+        boolean provider7=fast5gExtreme.equals(fast5gExtreme);
+        System.out.println(provider7);
         System.out.println(fast5gExtreme.toString());
         fast5gExtreme.service();
         
         System.out.println("running in provider");
         JioAirtelInternetProvider jioInternetProvider=new JioAirtelInternetProvider("sjfb", "sndb ");
-        boolean ref9=jioInternetProvider.equals(airtelInternetProvider);
-        System.out.println(ref9);
+        boolean provider8=jioInternetProvider.equals(airtelInternetProvider);
+        System.out.println(provider8);
         System.out.println(jioInternetProvider.toString());
         jioInternetProvider.service();
         
@@ -107,11 +109,13 @@ BookingProvider bookingProvider=new BookingProvider("jashb", "nsbdsvdv");
         
         
         SatJioAirtelInternetProvider satjioInternetProvider=new SatJioAirtelInternetProvider("smndbv", "dbv", 16," 14/09/2024");
-        boolean ref0=satjioInternetProvider.equals(jioInternetProvider);
-        System.out.println(ref0);
+        boolean provider9=satjioInternetProvider.equals(jioInternetProvider);
+        System.out.println(provider9);
         System.out.println(satjioInternetProvider.toString());
         satjioInternetProvider.service();
 
+        
+        
 	}
 
 }
